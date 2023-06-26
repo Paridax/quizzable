61ee4b8ef0c21c5718764846e5b650588639fc0c
Paridax <tgavdaly@gmail.com>
2023-06-25 20:08:59 -0400

    Update color theme

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,10 +1,14 @@
-<script lang='ts'>
-	// The ordering of these imports is critical to your app working properly
-	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+<script lang="ts">
+	// Your custom Skeleton theme:
+	import '../theme.postcss';
 	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
 </script>
 
-<slot />
+<div class="app min-h-screen w-full">
+	<main class="w-full">
+		<slot />
+	</main>
+</div>
