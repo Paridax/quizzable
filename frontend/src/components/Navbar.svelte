0ac0448a4e2499618766cbd5f8786ab60dc39152
Paridax <tgavdaly@gmail.com>
2023-06-27 21:41:26 -0400

    Create navbar
    Fixes #2

---
+++ frontend/src/components/Navbar.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { currentUser } from '$lib/pocketbase';
+	const guestLinks = {
+		Home: '/',
+		Login: '/login',
+		Register: '/register'
+	};
+
+	const userLinks = {
+		Home: '/',
+		Profile: '/profile',
+		Logout: '/logout'
+	};
+</script>
+
+<div class="w-full">
+	<nav class="flex items-center h-16 mx-auto container justify-between">
+		<h1>Quizzable</h1>
+		<ul class="flex items-center justify-between gap-5">
+			{#if $currentUser}
+				{#each Object.keys(userLinks) as link}
+					<li>
+						<a class="btn variant-soft-primary" href={userLinks[link]}>{link}</a>
+					</li>
+				{/each}
+			{:else}
+				{#each Object.keys(guestLinks) as link}
+					<li>
+						<a class="btn variant-soft-primary" href={guestLinks[link]}>{link}</a>
+					</li>
+				{/each}
+			{/if}
+		</ul>
+	</nav>
+</div>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -4,10 +4,12 @@
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of app CSS
 	import '../app.postcss';
+	import Navbar from '../components/Navbar.svelte';
 </script>
 
 <div class="app min-h-screen w-full">
 	<main class="w-full">
+		<Navbar />
 		<slot />
 	</main>
 </div>
