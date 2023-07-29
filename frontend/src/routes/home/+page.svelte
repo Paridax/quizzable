74e3b11890a2aa948e5471c2d64265d48123e269
Paridax <tgavdaly@gmail.com>
2023-07-28 22:20:00 -0400

    Add verified user display message

--- frontend/src/routes/home/+page.svelte
+++ frontend/src/routes/home/+page.svelte
@@ -7,5 +7,14 @@
 	<div class="space-y-5 py-10">
 		<h2 class="h2 font-bold w-full text-center">Welcome, {$currentUser?.displayName}!</h2>
         <h2 class="h3 font-semibold w-full text-center text-gray-500">@{$currentUser?.username}</h2>
+        {#if $currentUser?.verified === false}
+            <p class="w-1/2 text-center mx-auto text-warning-600-300-token">
+                You have not verified your email address yet. Please check your email for a verification link.
+            </p>
+        {:else}
+            <p class="w-1/2 text-center mx-auto p text-success-600-300-token">
+                You're verified!
+            </p>
+        {/if}
 	</div>
 </div>
