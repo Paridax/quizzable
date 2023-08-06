3eb44466e5dcdd532c3bcda17cba1d0c13e40e03
Paridax <tgavdaly@gmail.com>
2023-08-05 22:56:52 -0400

    Update base layout and title messages (broken ATM)

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -6,12 +6,41 @@
 	import '../app.postcss';
 	import Navbar from '../components/Navbar.svelte';
 	import Footer from '../components/Footer.svelte';
+	import { Toast, toastStore, type ToastSettings, autoModeWatcher } from '@skeletonlabs/skeleton';
+	import { onMount } from 'svelte';
+
+    const cookieToast: ToastSettings = {
+        message: 'We use cookies to improve your experience on our site. By using our site, you consent to cookies. <a href="/legal/cookies" class="anchor">Learn More</a>',
+	    timeout: 1000 * 1000, // 1000 seconds
+        hideDismiss: true,
+        action: {
+            label: 'I Understand',
+            response: () => {
+                if (window) {
+                    window.localStorage.setItem('cookies', 'accepted');
+                }
+            }
+        }
+    };
+
+    onMount(() => {
+        // Check if the user has accepted cookies
+        if (window && window.localStorage.getItem('cookies') === null) {
+            // If not, show the cookie consent banner
+            toastStore.trigger(cookieToast);
+        }
+    });
 </script>
 
+<svelte:head>{@html `<script>${autoModeWatcher.toString()} autoModeWatcher();</script>`}</svelte:head>
+<Toast />
+
 <div class="app min-h-screen w-full">
-	<main class="w-full">
+	<div class="w-full px-5">
 		<Navbar />
-		<slot />
+        <main>
+            <slot />
+        </main>
         <Footer />
-	</main>
+	</div>
 </div>
