760c16b54ba1b923c8db383551f2479f71b87e75
Paridax <tgavdaly@gmail.com>
2023-08-08 16:48:22 -0400

    Remove source code link from footer

--- frontend/src/components/Footer.svelte
+++ frontend/src/components/Footer.svelte
@@ -4,7 +4,6 @@
         <a class="text-sm text-surface-500-400-token hover:text-surface-600-300-token whitespace-nowrap" href="/legal/privacy">Privacy Policy</a>
         <a class="text-sm text-surface-500-400-token hover:text-surface-600-300-token whitespace-nowrap" href="/legal/cookies">Cookie Policy</a>
         <a class="text-sm text-surface-500-400-token hover:text-surface-600-300-token whitespace-nowrap" href="https://forms.gle/bJyiH9byk1E7w7Ug7">Provide Feedback</a>
-        <a class="text-sm text-surface-500-400-token hover:text-surface-600-300-token whitespace-nowrap" href="https://github.com/paridax/quizzable">Source Code</a>
         <a class="text-sm text-surface-500-400-token hover:text-surface-600-300-token whitespace-nowrap" href="mailto:contact@trevord.live">Contact</a>
         <p class="text-surface-500-400-token text-sm whitespace-nowrap">© 2023 <strong>Quizzable</strong> by <a class="anchor" href="https://trevord.live">Trevor D.</a></p>
     </footer>
