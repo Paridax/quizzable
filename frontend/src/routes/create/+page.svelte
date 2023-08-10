31694250a57db6db1429ee2eaa4984a8a0d78789
Paridax <tgavdaly@gmail.com>
2023-08-09 21:31:03 -0400

    Disallow unauthorized set creation

--- frontend/src/routes/create/+page.svelte
+++ frontend/src/routes/create/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import { TabGroup, Tab, TabAnchor } from '@skeletonlabs/skeleton';
+	import Loading from '../../components/Loading.svelte';
+	import { currentUser } from '$lib/pocketbase';
 
     const newSet = {
         name: '',
@@ -20,16 +22,20 @@
     let tabSet = 0;
 </script>
 
+{#if $currentUser}
 <main>
     <div class="container h-full mx-auto my-10">
         <h2 class="h2 font-semibold w-full text-center">Create a Quizzable</h2>
         <form method="POST" action="?/quiz" class="w-1/2 grid grid-cols-2 gap-5 mx-auto mt-10">
-            <button formaction="?/flashcards" class="card p-5 py-16 card-hover">
+            <button disabled={!$currentUser.verified} formaction="?/flashcards" class="card p-5 py-16 card-hover">
                 <h1 class="h3">Create Flashcards</h1>
             </button>
-            <button formaction="?/quiz" class="card p-5 py-16 card-hover">
+            <button disabled={!$currentUser.verified} formaction="?/quiz" class="card p-5 py-16 card-hover">
                 <h1 class="h3">Create a Quiz</h1>
             </button>
         </form>
     </div>
 </main>
+{:else}
+    <Loading />
+{/if}
