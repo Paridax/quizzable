d3ba88ecb93e269475d62ce0b14f86e7b9a91653
Paridax <tgavdaly@gmail.com>
2023-08-11 17:17:24 -0400

    Update initial types of currentUser

--- frontend/src/lib/pocketbase.ts
+++ frontend/src/lib/pocketbase.ts
@@ -1,7 +1,8 @@
 import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
 import PocketBase from 'pocketbase';
 import { writable } from 'svelte/store';
+import type { User } from './types';
 
 export const pb = new PocketBase(PUBLIC_POCKETBASE_URL);
 
-export const currentUser = writable(pb.authStore.model);
+export const currentUser = writable(pb.authStore.model ? pb.authStore.model as User : false);
